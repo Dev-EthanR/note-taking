1b617bdb486542f68453a06759bb98e7bc9c17a8
Ethan <ethanlegend7@gmail.com>
2026-03-21 15:52:05 +1100

    refactor: adjsut some small tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,23 @@ import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 interface Props {
-  params: {
-    name: string;
-  };
   searchParams: Promise<{ note?: string }>;
 }
 
-export default async function Home({ params, searchParams }: Props) {
+export default async function Home({ searchParams }: Props) {
   const session = await auth();
   const note = await searchParams;
 
   if (!session) redirect("/login");
   return (
-    <div className="lg:pl-8 flex flex-1 min-h-[calc(100vh-var(--navheader-height))]">
-      <div className="lg:border-r lg:border-neutral-300 pt-5 pr-4 min-h-[calc(100vh-var(--navheader-height))]">
+    <div className="lg:pl-8 flex flex-1 fill-to-height">
+      <div className="lg:border-r lg:border-neutral-300 pt-5 pr-4 fill-to-height">
         <CreateNoteButton
           style={note.note ? "invisible lg:visible" : "visible"}
         />
       </div>
       {note.note && (
-        <div className="pt-5 lg:px-6 lg:border-r lg:border-neutral-300 min-h-[calc(100vh-var(--navheader-height))]">
+        <div className="pt-5 lg:px-6 lg:border-r lg:border-neutral-300 fill-to-height">
           <CreateNote />
         </div>
       )}
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import NavLinks from "./Navlinks";
 const Navbar = () => {
   return (
     <>
-      <div className="lg:border-r lg:border-neutral-300  lg:w-68 lg:h-screen lg:px-4 lg:py-3 lg:gap-y-4">
+      <div className="lg:border-r lg:border-neutral-300  lg:w-68 lg:min-h-screen lg:px-4 lg:py-3 lg:gap-y-4">
         <NavHeader screen="desktop" />
         <NavLinks />
       </div>
