394f977c14951c31292ca789706af2732ef09e38
Ethan <ethanlegend7@gmail.com>
2026-03-23 13:13:58 +1100

    client side form for create note

--- components/notes/CreateNote.tsx
+++ components/notes/CreateNote.tsx
@@ -1,11 +1,29 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 import { Button } from "../ui/button";
 import CreateNoteHeader from "./CreateNoteHeader";
 
+interface Note {
+  title?: string;
+  tags?: string;
+  note?: string;
+}
+
 const CreateNote = () => {
+  const { handleSubmit, register } = useForm<Note>();
+
+  function onSubmit(noteData: Note) {
+    const title = noteData.title || "Untitled Note";
+    const tags = noteData.tags?.split(",");
+    const note = noteData.note;
+  }
   return (
-    <form className="flex flex-col lg:flex-col-reverse h-full">
+    <form
+      className="flex flex-col lg:flex-col-reverse h-full"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex justify-between border-b-2 pb-2 mb-2 border-neutral-200 lg:border-t-2 lg:border-b-0 lg:py-4 lg:my-2">
         <Link
           href="/"
@@ -20,15 +38,18 @@ const CreateNote = () => {
           Go Back
         </Link>
         <div className="lg:flex flex-row-reverse gap-2">
-          <Button
-            variant="link_button"
-            className="text-neutral-600 lg:bg-neutral-100 lg:hover:bg-neutral-200"
-          >
-            Cancel
-          </Button>
+          <Link href="/">
+            <Button
+              variant="link_button"
+              className="text-neutral-600 lg:bg-neutral-100 lg:hover:bg-neutral-200"
+            >
+              Cancel
+            </Button>
+          </Link>
           <Button
             variant="link_button"
             className="text-primary-500 lg:bg-primary-500 lg:text-white lg:hover:bg-primary-700"
+            type="submit"
           >
             Save Note
           </Button>
@@ -36,14 +57,20 @@ const CreateNote = () => {
       </div>
       <div className="flex flex-col space-y-4 h-[95%] lg:h-full flex-1">
         <input
-          defaultValue="Enter a title..."
-          className="border-none outline-0 text-neutral-950 text-2xl font-bold"
+          id="title"
+          placeholder="Enter a title..."
+          className="border-none outline-0 text-neutral-950 text-2xl font-bold placeholder:text-neutral-950"
+          {...register("title")}
+          aria-label="title"
         />
         <div className="grid grid-cols-[115px_minmax(220px,1fr)] gap-y-2 md:grid-cols-[115px_minmax(500,1fr)] border-b-2 pb-4 mb-4 border-b-neutral-200">
           <CreateNoteHeader icon="/images/icon-tag.svg" title="Tags">
             <textarea
+              id="tags"
               placeholder="Add tags separated by commas (e.g. Work, Planning)"
               className="text-xs md:text-sm border-none outline-0 resize-none"
+              {...register("tags")}
+              aria-label="tags"
             />
           </CreateNoteHeader>
           <CreateNoteHeader icon="/images/icon-clock.svg" title="Last edited">
@@ -53,8 +80,11 @@ const CreateNote = () => {
           </CreateNoteHeader>
         </div>
         <textarea
+          id="note"
           placeholder="Start typing your note here…"
           className="w-full text-xs md:text-sm border-none outline-0 resize-none flex-1 placeholder:text-neutral-950"
+          aria-label="note"
+          {...register("note")}
         />
       </div>
     </form>
