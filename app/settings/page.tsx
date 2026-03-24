7e1e9265b2e7e76aa570b0f0444cf82a66e499cc
Ethan <ethanlegend7@gmail.com>
2026-03-24 12:54:56 +1100

    add user email to settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,19 @@
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Settings = () => {
-  return <div>Settings</div>;
+const Settings = async () => {
+  const session = await auth();
+
+  if (!session) return redirect("/auth/login");
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id,
+    },
+  });
+  return <div>{user?.email}</div>;
 };
 
 export default Settings;
